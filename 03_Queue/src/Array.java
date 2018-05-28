93128c0e890db2c526c283995f03d02940327077
geekv <416388066@qq.com>
2018-05-28 21:24:37 +0800

    Add files via upload

---
+++ 03_Queue/src/Array.java
@@ -0,0 +1,139 @@
+﻿
+public class Array<E> {          //要在类中引入E
+    private E [] data;    
+    private int size;
+
+
+    //构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = (E[])new Object[capacity];          //如何对泛型实例对象
+        size = 0;
+    }
+    //无参构造函数，默认数组容量为capacity=10
+    public Array(){
+        this(10);
+    }
+    //查询元素个数
+    public int getSize(){
+        return size;
+    }
+    //查询容量
+    public int getCapacity(){
+        return data.length;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    //在所有元素末尾添加一个元素
+
+    public void addLast(E e){
+        add(size,e);
+    }
+    //在所有元素之间添加一个元素
+
+    public void addFirst(E e){
+        add(0,e);
+    }
+    //在数组中插入一个元素
+    public void add(int index,E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed,Require index >= 0");
+
+        if (size == data.length)
+            resize(2 * data.length);
+
+        for (int i = size-1; i >= index; i--)    
+            data[i + 1] = data[i];
+
+        data[index] = e;         
+        size++;
+    }
+    //获取某个元素
+    public E get(int index){
+        if(index <0 || index>=size)
+            throw new IllegalArgumentException("Get failed,Index is illegal");
+        return data[index];
+    }
+
+    //修改某个元素
+    public void set(int index,E e){
+        if(index <0 || index>=size)
+            throw new IllegalArgumentException("Get failed,Index is illegal");
+        data[index] = e;
+    }
+    //查找数组是否包含元素e
+    public boolean contains(E e){
+        for(int i = 0;i<size;i++){
+            if (data[i] == e)
+                return true;
+            }
+            return false;   
+    }
+    //查找数组中元素e所在的索引，如果不存在元素e，则返-1
+    public int find(E e){
+        for(int i = 0;i<size;i++){
+            if (e == data[i])
+                return i;
+        }
+        return -1;
+    }
+    //查看最后一个元素
+    public E getLast(){
+        return get(size-1);
+    }
+
+    public E getFrist(){
+        return get(0);
+    }
+
+    //从数组中删除index位置的元素，返回删除的元素
+    public E remove(int index){
+        if(index <0 || index>=size)
+            throw new IllegalArgumentException("Get failed,Index is illegal");
+        E res =data[index];
+        for (int i =index+1;i<size;i++)
+            data[i-1] = data[i];
+        size--;
+        data[size]=null;             //可要可不要，size指向的空间有值，垃圾回收器无法回收，用户也永远无法访问，故手动删除
+        if (size<data.length/4 && data.length/2 != 0)          //lazy方法降低复杂度震荡，所以等到元素为1/4空间时，才缩容量
+            resize(data.length/2);
+        return res;
+    }
+    //删除数组中第一个元素
+    public E removeFrist(){
+        return remove(0);
+    }
+    //删除数组中最后一个元素，返回删除元素
+    public E removeLast(){
+        return remove(size-1);   
+    }
+    //从数据中删除元素e
+    public E removeElement(E e){
+        int i=find(e);
+        return remove(i);
+    }
+
+
+    //查询数组元素
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array:size=%d ,capacity =%d \n",size,data.length));
+        res.append('[');
+        for (int i=0; i <size;i++){
+            res.append(data[i]);
+            if (i !=size-1)
+                res.append(",");
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+    private void resize(int newcapacity){
+        E[] newdata = (E[])new Object[newcapacity];           
+
+        for(int i = 0;i<size;i++)
+            newdata[i]=data[i];
+        data=newdata;
+    }
+}
---
+++ 04_Binary-Search-Tree/src/BST.java
@@ -0,0 +1,263 @@
+﻿import java.util.Queue;
+import java.util.LinkedList;
+public class BST<E extends Comparable<E>> {
+
+    private class Node{
+        public Node left,right;         //在内部就不用private
+        public E e;
+
+        public Node(E e){
+            this.e =e ;                                              
+            left=null;
+            right=null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST(){
+        root = null;
+        size=0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public void add(E e){
+        if(root==null){
+            root = new Node(e);
+            size++;
+        }
+        else
+            root =add(root,e);
+    }
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node,E e){
+//        if(e.equals(node.e)){               //因为是compare接口，所以要用equals
+//            return;
+//        }
+//        else if(e.compareTo(node.e)<0 && node.left == null){
+//            node.left =new Node(e);
+//            size++;
+//            return;
+//        }
+//        else if(e.compareTo(node.e)>0 && node.right == null){
+//            node.right=new Node(e);
+//            size++;
+//            return;
+//        }
+//
+//        if(e.compareTo(node.e)<0)               
+//            add(node.left,e);
+//        else
+//            add(node.right,e);
+        //换个思维，假设我们插入的是一颗空的二叉树
+        if(node == null){
+            size++;
+            return new Node(e);
+        }
+        if(e.compareTo(node.e)<0)
+            node.left = add(node.left,e);
+        else if(e.compareTo(node.e)>0)
+            node.right =add(node.right,e);
+        return node;
+    }
+
+    public boolean contains(E e){
+        return contains(root,e);  
+    }
+    private boolean contains(Node node,E e){
+        if(node==null)
+            return false;
+
+        if(e.compareTo(node.e)<0)
+            return contains(node.left,e);
+        else if(e.compareTo(node.e)>0)
+            return contains(node.right,e);
+        else
+            return true;
+    }
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null)
+            return;
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node==null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    public void levelOrder(){
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()){            //错误写成 :queue != null.
+            Node cur =queue.remove();
+            System.out.println(cur.e);
+            if(cur.left != null)           
+                queue.add(cur.left);
+            if(cur.right != null)
+                queue.add(cur.right);
+        }
+
+    }
+    //查找二分搜索树中最大和最小的值
+    public E minmum(){
+        if(size==0)                        //注意：root==null。可能树中有空的元素
+            throw new IllegalArgumentException("BST is empty");
+        return minmum(root).e;
+    }
+
+    private Node minmum(Node node){
+        if(node.left ==null)
+            return node;
+        return  minmum(node.left);//值得注意，不能用node代替node.left，错误：node.left=minmun(node.left)
+    }
+
+    public E maxmum(){
+        if(size==0)                        //注意：root==null。可能树中有空的元素
+            throw new IllegalArgumentException("BST is empty");
+
+        return maxmum(root).e;
+    }
+    private Node maxmum(Node node){
+        if(node.left ==null)
+            return node;
+
+        return  maxmum(node.left);        //值得注意，不能用node代替node.left
+    }
+
+
+
+
+    // 从二分搜索树中删除元素为e的节点
+    public void remove(E e){
+
+        root = remove(root,e);
+    }
+
+    private Node remove(Node node,E e){
+        if( node == null )
+            return null;
+
+        if(e.compareTo(node.e)<0){
+            node.left = remove(node.left,e);
+            return node;                            //如果要返回根的话，就node.left=remove（..），如果不用就直接return
+        }
+        else if(e.compareTo(node.e)>0){
+            node.right =remove(node.right,e);
+            return node;
+        }
+        else{
+            if(node.left ==null){
+               Node rightNode = node.right;
+               node.right=null;
+               size--;
+               return rightNode;
+            }
+            if(node.right==null){          
+                Node leftnode = node.left;
+                node.right=null;
+                size--;
+                return leftnode;
+            }                               
+            Node newnode = minmum(node.right);
+            newnode.right= removeMin(node.right);       
+            newnode.left=node.left;
+            node.right=node.left=null;
+            return newnode;
+
+
+        }
+
+    }
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    public E removeMin(){
+        E ret = minmum();
+        root = removeMin(root);
+        return ret;
+    }
+    // 删除掉以node为根的二分搜索树中值为e的节点, 递归算法
+    // 返回删除节点后新的二分搜索树的根
+
+    private Node removeMin(Node node){
+
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right=null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+
+    }
+    public E removeMax(){
+        E ret = maxmum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    private Node removeMax(Node node){
+        if(node.right == null){
+            Node leftnode = node.left;
+            node.right=null;
+            size--;
+            return leftnode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+    private void generateBSTString(Node node,int depth,StringBuilder res){
+        if(node ==null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+        res.append(generateDepthString(depth)+node.e +"\n");       //如何打印每个节点元素？
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth+1,res);
+    }
+    private String generateDepthString(int depth){
+        StringBuilder res =new StringBuilder();
+        for (int i =0;i<depth;i++)
+            res.append("--");
+        return res.toString();
+    }
+}
